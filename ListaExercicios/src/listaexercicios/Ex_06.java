a4f683ed1ca27b79579a2e7768ca1804fca8f10a
mjigormj <igorcruzsilva17@gmail.com>
2019-09-30 17:31:48 -0300

    vai

--- ListaExercicios/src/listaexercicios/Ex_06.java
+++ ListaExercicios/src/listaexercicios/Ex_06.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Ex_06 {
     public static void main(String[] args) {
-        int dia, mes = 0, ano, ab, diaatu, mesatu, anoatu, dpe=0, idade, dvv;
+        int dia, mes = 0, ano, ab, diaatu, mesatu, anoatu, dpe=0, dpa=0, idade, dvv;
         String a;
         
         /*dados de nascimento*/
@@ -20,51 +20,79 @@ public class Ex_06 {
         
         /*dados atuais*/
         
-        a = JOptionPane.showInputDialog (null, "Qual dia é hoje?");
+        a = JOptionPane.showInputDialog(null, "Qual dia é hoje?");
         diaatu = Integer.parseInt(a);
-        
-        a = JOptionPane.showInputDialog (null, "Qual e o mes atual?");
+
+        a = JOptionPane.showInputDialog(null, "Qual e o mes atual?");
         mesatu = Integer.parseInt(a);
-        
-        a = JOptionPane.showInputDialog (null, "Qual o ano atual?");
+
+        a = JOptionPane.showInputDialog(null, "Qual o ano atual?");
         anoatu = Integer.parseInt(a);
+
+        if (ano == anoatu  && idade5%4==0 && mes < 3) {
+            ab = 1;
+        } else {
         
-        ab = 2016 - ano;
+        ab = anoatu - ano;
         int cont = 0;
-        while (cont == 0){
-            if (ab%4==0){
-                cont ++;
-                ab = ab/4;
-            }else{ab++;}}
+        while (cont == 0) {
+            if (ab % 4 == 0) {
+                cont++;
+                ab = ab / 4;
+            } else {
+                ab++;
+            }
+        }}
             
         for(int i=1;i < mes;i++){
             switch(i){
-                case 1: dpe += 31;break;
+                case 1: dpe += 31;break;    /*Janeiro*/
                 case 2: dpe +=28;break;
                 case 3: dpe += 31;break;
                 case 4: dpe += 30;break;
                 case 5: dpe += 31;break;
-                case 6: dpe += 30;break;  /*junho*/
+                case 6: dpe += 30;break;  /*Junho*/
                 case 7: dpe += 31;break;
                 case 8: dpe += 31;break;
                 case 9: dpe += 30;break;
                 case 10: dpe += 31;break;
                 case 11: dpe += 30;break;
-                case 12: dpe += 31;break;}
+                case 12: dpe += 31;break;   /*Dezembro*/
+            }
+        }
+        
+        for(int i=1;i < mesatu;i++){
+            switch(i){
+                case 1: dpa += 31;break;    /*Janeiro*/
+                case 2: dpa +=28;break;
+                case 3: dpa += 31;break;
+                case 4: dpa += 30;break;
+                case 5: dpa += 31;break;
+                case 6: dpa += 30;break;  /*Junho*/
+                case 7: dpa += 31;break;
+                case 8: dpa += 31;break;
+                case 9: dpa += 30;break;
+                case 10: dpa += 31;break;
+                case 11: dpa += 30;break;
+                case 12: dpa += 31;break;   /*Dezembro*/
+            }
         }
             
-        System.out.println("Anos bissextos: "+ ab);
+        System.out.println("Anos bissextos: " + ab);
+
+        dpe += dia;
+        System.out.println("meses mais dias: " + dpe);
+
+        idade = anoatu - ano;
+        System.out.println("idade: " + idade);
+
+        System.out.println("dias deste ano " + dpa);
         
-            dpe += dia;
-        System.out.println("meses mais dias: "+ dpe);
-        
-            idade = anoatu - ano;
-            System.out.println("idade: "+ idade);
-            
-            dvv = idade * 365;
-            dvv -= dpe;
-            dvv += ab;
-            
-        System.out.println("dias vividos: "+ dvv);
+        dvv = idade * 365;
+        dvv -= dpe;
+        dvv += dpa;
+        dvv += ab;
+
+        System.out.println("dias vividos: " + dvv);
     }
 }
